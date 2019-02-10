33ccd2c0389be86206e9562c7a89e3dd8ae2a785
Even Engesland <evenengesland@gmail.com>
2019-02-10 03:17:07 +0100

    Create deltaTime.cs

---
+++ Time.deltaTime/deltaTime.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deltaTime : MonoBehaviour {
+  public float speed = 3.0f;
+  
+  public void Update() {
+    transform.Translate(speed * Time.deltaTime);
+  }
+}
