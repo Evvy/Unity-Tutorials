d46be7d2ed8aa1bd7bcecbf52c1949e476457cec
Even Engesland <evenengesland@gmail.com>
2019-02-10 03:38:01 +0100

    Update deltaTime.cs

--- Time.deltaTime/deltaTime.cs
+++ Time.deltaTime/deltaTime.cs
@@ -6,6 +6,6 @@ public class deltaTime : MonoBehaviour {
   public float speed = 3.0f;
   
   public void Update() {
-    transform.Translate(speed * Time.deltaTime);
+    transform.Translate(speed * Time.deltaTime, 0, 0);
   }
 }
